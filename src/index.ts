844419d8c008fc200603fd163182f16cb6e3e501
Mateja Petrovic <mateja176@gmail.com>
2024-11-12 13:52:42 +0100

    builds project using rolldown

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import { Hono } from 'hono';
 import { handle } from 'hono/vercel';
+import { message } from './message';
 
 const basePath: string = '/api';
 const app = new Hono().basePath(basePath);
 
 app.get('/', (c) => {
-  return c.json({ message: 'Hello Vercel!' });
+  return c.json({ message });
 });
 
 // https://vercel.com/docs/functions/quickstart#create-an-api-route
