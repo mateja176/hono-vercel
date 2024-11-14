8ba194386c767b10e0d163ca3139fb7000afc797
Mateja Petrovic <mateja176@gmail.com>
2024-11-14 14:35:22 +0100

    adds cors middleware

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import { Hono } from 'hono';
+import { cors } from 'hono/cors';
 import { handle } from 'hono/vercel';
 import { message } from './message';
 
 const app = new Hono();
+
+app.use(cors());
 
 app.get('/', (c) => {
   return c.json({ message: `Hello from ${process.env.VERCEL_REGION}` });
